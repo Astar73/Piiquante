e84d0f962acc65fb6f5d80dca183c5c7b26b5337
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-09 17:29:18 +0100

    Ajout de commentaire sur les fonctions, modification du middleware d'authentification

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,15 +1,25 @@
+// Improtation de JsonWebToken
 const jwt = require('jsonwebtoken');
 
+// Middleware d'authentification de l'utilisateur
 module.exports = (req, res ,next) => {
     try {
+        // Récupère le token de la requête
         const token = req.headers.authorization.split(' ')[1];
+        // Vérifie si la clé d'auuthentification est la bonne
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        // Récupère l'userID du token
         const userId = decodedToken.userId;
-        req.auth = {
-            userId: userId
-        };
+        // Vérifie si l'userId est le même que dans la requête
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Identifiant utilisateur invalide';
+        }
+        // Autorise la requête
+        else {
+            next();
+        }
     }
     catch(error) {
-        res.status(401).json({ error });
+        res.status(401).json({ error: new Error('Requête invalide') })
     }
 };
