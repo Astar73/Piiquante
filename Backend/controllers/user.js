74ac1dd06af8e59ec41ccdc6b4cc037d259a81a3
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-09 19:35:29 +0100

    Réorganisation code

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -18,7 +18,7 @@ exports.signup = (req, res, next) => {
             // Enregistre l'utilisateur dans la base de données
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
-                .catch(error => res.status(400).json({ error })) ;      
+                .catch(error => res.status(400).json({ error }));      
         })
         .catch(error => res.status(500).json({ error }));
 };
@@ -31,30 +31,26 @@ exports.login = (req, res, next) => {
         // Si l'utilisateur n'éxiste pas
         if (!user) {            
             res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' }); /* On n'indique pas si l'utilisateur existe déjà où non dans la base de données par soucis de confidentialité */
-        }
-        else {
-            // Vérifie que le mot de passe est identique à celui de la base de données
-            bcrypt.compare(req.body.password, user.password)           
+        }        
+        // Vérifie que le mot de passe est identique à celui de la base de données
+        bcrypt.compare(req.body.password, user.password)           
             .then(valid => {
                 // Si le mot de passe n'est pas valide
                 if (!valid) {
-                    res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
+                    res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
                 }
-                else {
-                    res.status(200).json({
-                        userId: user._id,
-                        // Création d'un token d'authentification aléatoire
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            // Définit l'expiration du token dans 24h
-                            { expiresIn: '24h' }
-                        )
-                    });
-                }
+                res.status(200).json({
+                    userId: user._id,
+                    // Création d'un token d'authentification aléatoire
+                    token: jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        // Définit l'expiration du token dans 24h
+                        { expiresIn: '24h' }
+                    )
+                });                
             })
-            .catch(error => res.status(500).json({ error }))
-        }
+            .catch(error => res.status(500).json({ error }));     
     })
-    .catch(error => res.status(500).json({ error }))
+    .catch(error => res.status(500).json({ error }));
 };
