bf5fb37038e9fc9567f94aa7e13150bccb78ac63
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-09 17:00:49 +0100

    Ajout de commentaires sur les fonctions  + mise en forme du code

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,15 +1,21 @@
+// Importation de bcrypt
 const bcrypt = require('bcrypt');
+// Importation de JsonWebToken
 const jwt = require('jsonwebtoken');
 
+// Importation du modèle utilisateur
 const User = require('../models/User');
 
+// Contrôleur de création d'un utilisateur
 exports.signup = (req, res, next) => {
+    // Cryptage du mot de passe avec bcrypt
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
                 email: req.body.email,
                 password: hash
             });
+            // Enregistre l'utilisateur dans la base de données
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur crée !' }))
                 .catch(error => res.status(400).json({ error })) ;      
@@ -17,35 +23,38 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+// Contrôleur de connexion d'un utilisateur
 exports.login = (req, res, next) => {
+    // Vérifie si l'utilisateur existe déjà
     User.findOne({ email: req.body.email })
     .then(user => {
-        if (user === null) {
-            res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' });
+        // Si l'utilisateur n'éxiste pas
+        if (!user) {            
+            res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' }); /* On n'indique pas si l'utilisateur existe déjà où non dans la base de données par soucis de confidentialité */
         }
         else {
-            bcrypt.compare(req.body.password, user.password)
+            // Vérifie que le mot de passe est identique à celui de la base de données
+            bcrypt.compare(req.body.password, user.password)           
             .then(valid => {
+                // Si le mot de passe n'est pas valide
                 if (!valid) {
                     res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte' })
                 }
                 else {
                     res.status(200).json({
                         userId: user._id,
+                        // Création d'un token d'authentification aléatoire
                         token: jwt.sign(
                             { userId: user._id },
                             'RANDOM_TOKEN_SECRET',
+                            // Définit l'expiration du token dans 24h
                             { expiresIn: '24h' }
                         )
                     });
                 }
             })
-            .catch(error => {
-                res.status(500).json({ error });
-            })
+            .catch(error => res.status(500).json({ error }))
         }
     })
-    .catch(error => {
-        res.status(500).json({ error });
-    })
+    .catch(error => res.status(500).json({ error }))
 };
