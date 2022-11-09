4b06260de89ebf467e5ba41e71114e601d28cdea
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-09 17:36:27 +0100

    Ajout de commentaires

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,11 +1,16 @@
+// Importation de Mongoose
 const mongoose = require('mongoose');
+// Importation de Mongoose Unique Validator
 const uniqueValidator = require('mongoose-unique-validator');
 
+// Schéma de donnée pour un utilisateur sur mongoDB
 const userSchema = mongoose.Schema({
     email: { type : String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
+// Validation mongoose pour avoir un email unique
 userSchema.plugin(uniqueValidator);
 
+// Exporte le schéma de donnée
 module.exports = mongoose.model('User', userSchema);
