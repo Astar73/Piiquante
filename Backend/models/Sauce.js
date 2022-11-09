512be2b0a573d7fff7504575ac12a4d343a33d7a
Axel PERRET <109222569+Astar73@users.noreply.github.com>
2022-11-09 19:23:06 +0100

    Suppression des required sur les likes et dislikes

--- Backend/models/Sauce.js
+++ Backend/models/Sauce.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Schéma de donnée pour une sauce sur mongoDB
-const SauceSchema = mongoose.Schema({
+const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true }, // l'identifiant MongoDB unique de l'utilisateur qui a créé la sauce
     name: { type: String, required: true }, // nom de la sauce
     manufacturer: { type: String, required: true }, // fabricant de la sauce
@@ -10,11 +10,11 @@ const SauceSchema = mongoose.Schema({
     mainPepper: { type: String, required: true }, // le principal ingrédient épicé de la sauce
     imageUrl: { type: String, required: true }, // l'URL de l'image de la sauce téléchargée par l'utilisateur
     heat: { type: Number, required: true }, // nombre entre 1 et 10 décrivant la sauce
-    likes: { type: Number, default: 0, required: true }, // nombre d'utilisateurs qui aiment (= likent) la sauce
-    dislikes: { type: Number, default: 0, required: true }, // nombre d'utilisateurs qui n'aiment pas (= dislike) la sauce
-    usersLiked: { type: [String], required: true }, // tableau des identifiants des utilisateurs qui ont aimé (= liked) la sauce
-    usersDisliked: { type: [String], required: true }, // tableau des identifiants des utilisateurs qui n'ont pas aimé (= disliked) la sauce
+    likes: { type: Number, default: 0 }, // nombre d'utilisateurs qui aiment (= likent) la sauce
+    dislikes: { type: Number, default: 0 }, // nombre d'utilisateurs qui n'aiment pas (= dislike) la sauce
+    usersLiked: { type: [String] }, // tableau des identifiants des utilisateurs qui ont aimé (= liked) la sauce
+    usersDisliked: { type: [String] }, // tableau des identifiants des utilisateurs qui n'ont pas aimé (= disliked) la sauce
 });
 
 // Exporte le schéma de donnée
-module.exports = mongoose.model('Sauce', SauceSchema);
+module.exports = mongoose.model('Sauce', sauceSchema);
